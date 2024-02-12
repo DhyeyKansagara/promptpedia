0d5fce8d37d8571e365a0a1ec6e0400623d9aec6
DhyeyKansagara <DHYEYKANSAGARA@GMAIL.COM>
2024-02-11 22:44:51 -0800

    added key features

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,16 +8,17 @@ import {signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, settoggleDropdown] = useState(false)
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUPProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
+    setUPProviders();
   }, [])
 
   return (
@@ -32,9 +33,11 @@ const Nav = () => {
         <p className='logo_text'> Promptpedia</p>
     </Link>
 
+
+
     {/* Desktop Navigation */}
     <div className='sm:flex hidden'>
-      {isUserLoggedIn ? (
+      {session?.user ? (
         <div className='flex gap-3 md:gap-5'>
           <Link href='/create-prompt' className='black_btn'>Create Post</Link>
           <button type="button" onClick = {signOut} className='outline_btn'>
@@ -42,7 +45,7 @@ const Nav = () => {
           </button>
           <Link href="/profile">
             <Image 
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
@@ -70,10 +73,10 @@ const Nav = () => {
 
     {/* Mobile Navigation */}
     <div className='sm:hidden flex relative'>
-      {isUserLoggedIn ? (
+      {session?.user ? (
           <div className='flex'>
             <Image 
-                src="/assets/images/logo.svg"
+                src= {session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -91,6 +94,20 @@ const Nav = () => {
                   >
                     My Profile
                   </Link>
+                  <Link 
+                  href="/create-prompt"
+                  className='dropdown_link'
+                  onClick={() => settoggleDropdown(false)}                  
+                  >
+                    Create Prompt
+                  </Link>
+                  <button
+                  type='button' 
+                  onClick={()=> {settoggleDropdown(false); signOut();}} 
+                  className='mt-5 w-full black_btn'
+                  >
+                  Sign Out
+                  </button>
                 </div>
               )}
           </div>
